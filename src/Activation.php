0de8b27bcd15ca55ef38a23fe7acfa012fb2f592
ScurraMagna <55872534+ScurraMagna@users.noreply.github.com>
2020-12-16 12:19:39 +0000

    Create Activation.php

---
+++ src/Activation.php
@@ -0,0 +1,38 @@
+<?php
+
+class Activation {
+
+  public function tanh ($a) {
+    return tanh($a);
+  }
+
+  public function dtanh ($a) {
+    return 1 - $a * $a;
+  }
+
+  public function sigmoid ($a) {
+    return 1 / (1 + exp(-$a))
+  }
+
+  public function dsigmoid ($a) {
+    return $a * (1 - $a);
+  }
+
+  public function ReLU ($a) {
+    return $a < 0 ? 0 : $a;
+  }
+
+  public function dReLU ($a) {
+    return $a < 0 ? 0 : 1;
+  }
+
+  public function switch ($a) {
+    return $a * $this->sigmoid($a);
+  }
+
+  public function dswitch ($a) {
+    return $this->sigmoid($a) * (1 - $a) + $a;
+  }
+}
+
+?>
